a0976e48f716b6470d9a17e7b94d9c58df66339f
Michael Ripley <zkxs00@gmail.com>
2022-05-01 05:47:16 -0500

    Initial commit

---
+++ NeosVoiceQuality/VoiceQuality.cs
@@ -0,0 +1,14 @@
+﻿using NeosModLoader;
+
+namespace VoiceQuality
+{
+    public class VoiceQuality : NeosMod
+    {
+        public override string Name => "VoiceQuality";
+        public override string Author => "runtime";
+        public override string Version => "1.0.0";
+        public override string Link => "https://github.com/zkxs/NeosVoiceQuality";
+
+        
+    }
+}
