7b514a4f295cabeb52247ef6344eb0a424d7d666
Michael Ripley <zkxs00@gmail.com>
2022-05-01 09:10:59 -0500

    Make the mod

--- NeosVoiceQuality/VoiceQuality.cs
+++ NeosVoiceQuality/VoiceQuality.cs
@@ -1,4 +1,12 @@
-﻿using NeosModLoader;
+﻿//#define DEBUG_SPAM // enable this for incredible debug spam
+
+using CodeX;
+using FrooxEngine;
+using HarmonyLib;
+using NeosModLoader;
+using System;
+using System.Linq;
+using System.Reflection;
 
 namespace VoiceQuality
 {
@@ -9,6 +17,177 @@ namespace VoiceQuality
         public override string Version => "1.0.0";
         public override string Link => "https://github.com/zkxs/NeosVoiceQuality";
 
-        
+        private static readonly Type USER_TYPE = typeof(User);
+        private static readonly string USER_PATCH_TARGET = "PostInitializeWorker";
+        private static readonly Type STREAM_TYPE = typeof(OpusStream<MonoSample>);
+
+        // neos's default bitrate is 25000
+        // neos clamps bitrate between 2400 and 500000
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<int> KEY_BITRATE = new ModConfigurationKey<int>("bitrate", "Bitrate to use for your audio stream. 25000 is default.", () => 25000);
+
+        [AutoRegisterConfigKey]
+        private static readonly ModConfigurationKey<bool> KEY_AUTO_SET_NEW_USERS = new ModConfigurationKey<bool>("autoset", "Automatically set bitrate when joining new worlds?", () => true);
+
+        private static ModConfiguration? config;
+
+        public override void DefineConfiguration(ModConfigurationDefinitionBuilder builder)
+        {
+            builder
+                .Version(new Version(1, 0, 0))
+                .AutoSave(false);
+        }
+
+        public override void OnEngineInit()
+        {
+            config = GetConfiguration();
+            if (config == null)
+            {
+                Error("Could not load configuration");
+                return;
+            }
+
+            Harmony harmony = new Harmony("dev.zkxs.NeosVoiceQuality");
+
+            config.OnThisConfigurationChanged += OnConfigurationChanged;
+
+            InstallPatch(harmony, USER_TYPE, USER_PATCH_TARGET);
+        }
+
+        // handle changes to the bitrate config
+        private void OnConfigurationChanged(ConfigurationChangedEvent @event)
+        {
+            try
+            {
+                if (KEY_BITRATE.Equals(@event.Key))
+                {
+                    foreach (World world in Engine.Current.WorldManager.Worlds)
+                    {
+                        if (!world.IsUserspace())
+                        {
+                            HandleExistingUser(world.LocalUser);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Error($"Something went very wrong in OnConfigurationChanged():\n{e}");
+            }
+        }
+
+        // runs on config change
+        private static void HandleExistingUser(User user)
+        {
+            OpusStream<MonoSample> stream;
+            try
+            {
+                stream = (OpusStream<MonoSample>)user.Streams.Single(stream => STREAM_TYPE.IsAssignableFrom(stream.GetType()));
+            }
+            catch (InvalidOperationException)
+            {
+                Error($"There was not exactly one {STREAM_TYPE} stream in user {user}");
+                DebugUserStreams(user);
+                return;
+            }
+
+            user.World.RunSynchronously(() => {
+                int bitrate = config!.GetValue(KEY_BITRATE);
+                stream.BitRate.Value = bitrate;
+                Msg($"set bitrate to {bitrate} in {user.World?.Name} for existing user");
+            });
+        }
+
+        private static void DebugUserStreams(User user)
+        {
+            foreach(Stream someStream in user.Streams)
+                {
+                Debug($"  - {someStream.GetType()}");
+            }
+        }
+
+        // runs when a new stream is added to any user
+        private static void HandleNewStream(Stream stream)
+        {
+#if DEBUG_SPAM
+            Debug("HandleNewStream() call");
+#endif
+
+            if (stream is OpusStream<MonoSample> opusStream)
+            {
+                // okay, we're in business, but we have some checks to do first
+                if (stream.User == null || stream.World == null)
+                {
+                    return;
+                }
+                User user = stream.User;
+                if (!user.IsLocalUser)
+                {
+                    // this is someone else... lets not touch them!
+                    return;
+                }
+                World world = stream.World;
+                if (world.IsUserspace())
+                {
+                    // nobody talks in userspace
+                    return;
+                }
+
+                world.RunSynchronously(() => {
+                    int bitrate = config!.GetValue(KEY_BITRATE);
+                    opusStream.BitRate.Value = bitrate;
+                    Msg($"set bitrate to {bitrate} in {stream.World?.Name} for new stream");
+                });
+            }
+        }
+
+        private static void InstallPatch(Harmony harmony, Type type, string methodName)
+        {
+            MethodInfo method = AccessTools.Method(type, methodName);
+            if (method == null)
+            {
+                throw new ReflectionException($"Could not find {type.Name}.{methodName}()");
+            }
+            MethodInfo @base = method.GetBaseDefinition();
+            harmony.Patch(@base, postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.UserInitPostfix)));
+        }
+
+        // keep those pesky harmony methods separate from my normal methods
+        private static class HarmonyPatches
+        {
+            // postfix patch to User.PostInitializeWorker()
+            public static void UserInitPostfix(object __instance, MethodBase __originalMethod)
+            {
+                if (USER_TYPE.Equals(__instance.GetType()))
+                {
+#if DEBUG_SPAM
+                    Debug($"Postfix call: [{__originalMethod.DeclaringType} / {__originalMethod}] on [{__instance.GetType()}]");
+#endif
+
+                    if (__instance is User user)
+                    {
+                        DebugUserStreams(user);
+                        if (user.StreamCount != 0)
+                        {
+                            HandleExistingUser(user);
+                        }
+                        else
+                        {
+                            Debug($"mystery user looks new... lets try adding a listener?");
+                            user.StreamAdded += HandleNewStream;
+                        }
+                    }
+                    else
+                    {
+                        Error("Something has gone very wrong, and an object that should have been a User was not a User");
+                    }
+                }
+            }
+        }
+
+        private class ReflectionException : Exception
+        {
+            public ReflectionException(string message) : base(message) { }
+        }
     }
 }
--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod that allows you to easily change your own voice stream's bitrate.
 
+It has two configs:
+- **bitrate**: The bitrate to use for all of your audio streams. Changing this will immediately update your bitrate in *all* worlds you're in, including unfocused worlds.
+- **autoset**: If true, your bitrate will be automatically updated to your configured value upon joining new worlds.
+
 ## Installation
 1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
 2. Place [VoiceQuality.dll](https://github.com/zkxs/NeosVoiceQuality/releases/latest/download/VoiceQuality.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
